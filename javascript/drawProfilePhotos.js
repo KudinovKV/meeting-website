b92e0bf3463d221a15bf070b9758c9c21afc84f0
KudinovKV <48416282+KudinovKV@users.noreply.github.com>
2019-11-27 23:54:21 +0300

    New version

--- javascript/drawProfilePhotos.js
+++ javascript/drawProfilePhotos.js
@@ -1,142 +1,183 @@
 
 
 var ID;
+var CSRFTOCKEN;
+var USERIMAGEBYTE;
 
-$.ajax({
-    type: "POST",
-    url: "futurepartner-handler.php",
-    data:"ACTION=GETID",
-    success:function(id)
-    {
-        var idToShow = Number(id)
-        if(isNaN(idToShow))
-        {
-            // This is error
-            alert(id);
-            window.location.href = "index.php";
-        }
-        ID = id;
-        getPaths(id);
-    }
-});
+CreateProfileForm();
 
-
-
-var currentSlide = -1;
-
-$('.futurepartner').on('swipe', function(event, slick, direction){
-    // Get the current slide
-    var nextSlide = $('.futurepartner').slick('slickCurrentSlide');
-    if(currentSlide == -1 )
-    {
-        currentSlide = nextSlide;
-    }
-    else if(currentSlide == nextSlide && direction == 'left')
-    {
-        //window.location.href = "like.php";
-        SafeSwipe('LIKE');
-    }
-    else if(currentSlide == nextSlide && direction == 'right')
-    {
-        SafeSwipe('DISLIKE');
-    }
-    else{
-      currentSlide = nextSlide;
-    }
-});
+var currentSlide = 0;
 
 function SafeSwipe(action){
-    $.ajax({
-    type: "POST",
-    url: "futurepartner-handler.php",
-    data:{
-        ACTION:"SETSWIPE",
-        SAFESWIPE:action,
-        CURRENTID:ID
-    },
-    success:function(msg)
+    if (action == 'LIKE')
     {
-        if(msg == 'Correct')
+        $.ajax(
         {
-            // Доделать
-            if(action == 'LIKE'){
-                alert('You have a match with' . msg);
+            type: "POST",
+            url: "api/index.php",
+            data:{
+                apiandfunctionname:"apiphoto_like",
+                csrftocken: CSRFTOCKEN,
+                genid:ID
+            },
+            success:function(Answer)
+            {
+                Msg = JSON.parse(Answer);
+                if(!$.isEmptyObject(Msg['response']['result']))
+                {
+                    if(Msg['response']['result'] == 'MATCH')
+                    {
+                        alert('Congratulation! You have a match!');
+                        UpdateProfileForm();
+                        CreateProfileForm();
+                    }
+                    else {
+                        // Uncorrect action => GoNext
+                        UpdateProfileForm();
+                        CreateProfileForm();    
+                    }
+                }
+                else if(!$.isEmptyObject(Msg['ERROR']))
+                {
+                    alert(Msg['ERROR']);
+                    UpdateProfileForm();
+                    CreateProfileForm();
+                }
+                else{
+                    // Uncorrect answer => GoNext
+                    UpdateProfileForm();
+                    CreateProfileForm();
+                }
             }
+        });
+    }
+    else
+    {
+        UpdateProfileForm();
+        CreateProfileForm();    
+    }
+}
+
+function CreateProfileForm(){
+    $.ajax({
+        type: "POST",
+        url: "api/index.php",
+        data:"apiandfunctionname=apiauth_getcsrftocken",
+        success:function(jsonObj)
+        {
+            result = JSON.parse(jsonObj);
+            CSRFTOCKEN = result['response']['csrftocken'];
+            drawProfilePhotos();
+        }
+    });
+}
+
+function UpdateProfileForm(){
+    // Delete div
+    var old_div_futurepartner = document.getElementById('main');
+    old_div_futurepartner.remove();
+    // Create new
+    var fathermain = document.getElementById('fathermain');
+
+    var div_futurepartner = document.createElement("div");
+    div_futurepartner.className = "futurepartner";
+    div_futurepartner.setAttribute("id", "main");
+    
+    fathermain.appendChild(div_futurepartner);
+}
+
+
+function initSlider(){
+    $('.futurepartner').slick(
+    {
+        adaptiveHeight: false,
+        variableWidth: true,
+        autoplay: false,
+        dots: false,
+        arrows: false,
+        centerMode: true,
+        infinite: false,
+        swipe: true,
+        edgeFriction: 0.2
+    });
+
+    $('.futurepartner').on('swipe', function(event, slick, direction){
+        // Get the current slide
+        var nextSlide = $('.futurepartner').slick('slickCurrentSlide');
+        if(currentSlide == nextSlide && direction == 'left')
+        {
+            //window.location.href = "like.php";
+            SafeSwipe('LIKE');
+            currentSlide = 0;
+        }
+        else if(currentSlide == nextSlide && direction == 'right')
+        {
+            SafeSwipe('DISLIKE');
+            currentSlide = 0;
         }
         else{
-            // Error
-            alert(msg);
+          currentSlide = nextSlide;
         }
-    }
     });
 }
 
 
-
-function initSlider(){
-    $('.futurepartner').slick({
-    adaptiveHeight: false,
-    variableWidth: true,
-    autoplay: false,
-    dots: true,
-    arrows: false,
-    centerMode: true,
-    infinite: false,
-    swipe: true,
-    edgeFriction: 0.5
-});
-}
-
-function getPaths(id){
-    $.ajax({
-            type: "POST",
-            url: "futurepartner-handler.php",
-            data:{
-                ACTION: "GETPATHS" ,
-                ID: id
-            },
-            success:function(PathsToShow)
+function drawProfilePhotos() {
+    $.post(
+        "api/index.php",
+        {
+            apiandfunctionname: "apiphoto_getphoto" ,
+            version           : "brawser",
+            selectid          : "0",
+            csrftocken        : CSRFTOCKEN
+        },
+        function(Information,status)
             {
-                CorrectPaths = JSON.parse(PathsToShow);
-                // TODO : Добавить проверку полученного значения
-                drawProfilePhotos(id , CorrectPaths , Object.keys(CorrectPaths).length);
+                var div_futurepartner = document.getElementById("main");
+                var UserObject = JSON.parse(Information);
+                var userimages = UserObject['response']['images'];
+                var result = UserObject['response']['result'];
+                ID = UserObject['response']['genid'];
+                if (result != "Failed")
+                {
+                    userimages.forEach(function(image)
+                    {
+                        var div_slide = document.createElement("div");
+                        div_slide.className = "slide";
+                        div_futurepartner.appendChild(div_slide);
+                    
+                        var div_slideimage = document.createElement("div");
+                        div_slideimage.className = "slideimage";
+                        div_slide.appendChild(div_slideimage);
+                        var img = document.createElement("img");
+                        img.src = 'data:image/jpeg;base64,' + image;
+                        div_slideimage.appendChild(img);
+
+                        var div_slidetext = document.createElement("div");
+                        div_slidetext.className = "slidetext";
+                        div_slide.appendChild(div_slidetext);
+
+                        var div_slideheading = document.createElement("h3");
+                        div_slideheading.className = "slideheading";
+                        div_slidetext.appendChild(div_slideheading);
+                        div_slideheading.innerHTML = UserObject['response']['firstname'] + ' ' + UserObject['response']['lastname'];
+
+                        var div_slidedescription = document.createElement("p");
+                        div_slidedescription.className = "slidedescription";
+                        div_slidetext.appendChild(div_slidedescription);
+                        div_slidedescription.innerHTML = UserObject['response']['aboutyou'];                    
+
+                    });
+                    initSlider();
+                 }
+                 else
+                 {
+                    alert('Sorry! There are nothing for you yet!');
+
+                 }
+
             }
-      });
+                // TODO : Добавить проверку полученного значения
+      );
 }
 
-
-function drawProfilePhotos(CurrentID , Paths , Length) {
-    var div_futurepartner = document.getElementById("main");
-
-    var keys = Object.keys(Paths);
-    for (var key in keys) 
-    {
-        // image
-        var div_slide = document.createElement("div");
-        div_slide.className = "slide";
-        div_futurepartner.appendChild(div_slide);
-    
-        var div_slideimage = document.createElement("div");
-        div_slideimage.className = "slideimage";
-        div_slide.appendChild(div_slideimage);
-        var img = document.createElement("img");
-        img.src = 'images/'+ CurrentID + '/' + Paths[keys[key]];
-        div_slideimage.appendChild(img);
-        // description
-        var div_slidetext = document.createElement("div");
-        div_slidetext.className = "slidetext";
-        div_slide.appendChild(div_slidetext);
-
-        var div_slideheading = document.createElement("h3");
-        div_slideheading.className = "slideheading";
-        div_slidetext.appendChild(div_slideheading);
-        div_slideheading.innerHTML = "Slide " + key;
-
-        var div_slidedescription = document.createElement("p");
-        div_slidedescription.className = "slidedescription";
-        div_slidetext.appendChild(div_slidedescription);
-        div_slidedescription.innerHTML = "Description slide " + key;
-    }
-    
-    initSlider();
-}
